77db4bacf635992914e694351fbc2519a8eaacb5
Paul Bond <paulandrewbond@outlook.com>
2020-02-15 18:09:24 +0000

    add classname to topic title

--- src/components/Topics/SingleTopicPage.jsx
+++ src/components/Topics/SingleTopicPage.jsx
@@ -5,7 +5,7 @@ export default function SingleTopicPage(props) {
   const { loggedInUser, topic_slug } = props;
   return (
     <main>
-      <h2>/{topic_slug}</h2>
+      <h2 className="topic-title">/{topic_slug}</h2>
       <ArticleList loggedInUser={loggedInUser} topic_slug={topic_slug} />
     </main>
   );
